f73b3bb8448e7f819f49cdd42d1ee24e33b91226
dcastrop <d.castro-perez@imperial.ac.uk>
2019-09-19 10:33:08 +0100

    Fixed:
      - Function merge in main.c.
      - Prim now always introduces a new declaration in header file.

--- examples/Mergesort/Mergesort.hs
+++ examples/Mergesort/Mergesort.hs
@@ -19,23 +19,7 @@ seqMsort :: [Int] :-> [Int]
 seqMsort = fix msort
 
 parMsort :: [Int] :=> [Int]
-parMsort = annotate strat $ kfix 1 msort
+parMsort = annotate strat $ kfix 4 msort
 
 strat :: AnnStrat
 strat = ann seqMsort
-
---mrg :: CAlg f => f (T [Int]) [Int]
---mrg =
---  id ||| prim "merge"
---mrg = prim "merge"
---
---ms :: CArrChoice t => t [Int] [Int] -> t [Int] [Int]
---ms msr = spl >>> (id +++ msr *** msr) >>> mrg
---
---msort :: [Int] :-> [Int]
---msort = fix ms
---
---pms :: [Int] :=> [Int]
---pms = annotate annot $ kfix 4 ms
---  where
---    annot = ann spl <> ann msort
--- examples/Mergesort/main.c
+++ examples/Mergesort/main.c
@@ -1,24 +1,5 @@
 #include "Mergesort.h"
 
-//either_vec_int_pair_vec_int_vec_int_t split(vec_int_t in){
-//  either_vec_int_pair_vec_int_vec_int_t out;
-//  if (in.size <= 1) {
-//    out.tag = Inl;
-//    out.val.inl = in;
-//  } else {
-//    out.tag = Inr;
-//    vec_int_t in1;
-//    vec_int_t in2;
-//    in2.size = in.size - in1.size;
-//    in2.elems = in.elems + in1.size;
-//    out.val.inr.fst.size = in.size / 2;
-//    out.val.inr.fst.elems = in.elems;
-//    out.val.inr.snd.size = in.size - out.val.inr.fst.size;
-//    out.val.inr.snd.elems = in.elems + out.val.inr.fst.size;
-//  }
-//  return out;
-//}
-//
 vec_int_t merge(pair_vec_int_vec_int_t in){
   vec_int_t out;
   int sz = in.fst.size + in.snd.size;
@@ -33,8 +14,8 @@ vec_int_t merge(pair_vec_int_vec_int_t in){
         tmp[z] = in.snd.elems[j++];
       }
     }
-    if (i < in.fst.size) tmp[z] = in.fst.elems[i++];
-    if (j < in.snd.size) tmp[z] = in.snd.elems[j++];
+    else if (i < in.fst.size) tmp[z] = in.fst.elems[i++];
+    else if (j < in.snd.size) tmp[z] = in.snd.elems[j++];
   }
   out = in.fst;
   out.size = sz;
@@ -45,32 +26,11 @@ vec_int_t merge(pair_vec_int_vec_int_t in){
   return out;
 }
 
-//vec_int_t merge(either_vec_int_pair_vec_int_vec_int_t in){
-//  vec_int_t out;
-//  switch (in.tag) {
-//    case Inl:
-//      { out = in.val.inl;
-//        break;
-//      }
-//    case Inr:
-//      { int sz = in.val.inr.fst.size + in.val.inr.snd.size;
-//        out = do_merge(sz, in.val.inr.fst, in.val.inr.snd);
-//      }
-//  }
-//  return out;
-//}
-//
 int main(void) {
   vec_int_t in;
   int inv[] = {3,1,2,31,4,1,4,5,6,1,6,1,2,1,2,3,4,5,1,5,77,5,2,4};
   in.elems = (int *)inv;
   in.size = sizeof(inv) / sizeof(int);
-
-  // vec_int_t sout = fix_fun(in);
-  // for (int i = 0; i < sout.size; i++) {
-  //   printf("%d ", sout.elems[i]);
-  // }
-  // printf("\n");
 
   vec_int_t out = parMsort(in);
   for (int i = 0; i < out.size; i++) {
