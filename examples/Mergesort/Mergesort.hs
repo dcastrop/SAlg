717b00ccef5df2faf69cb312c2290053bb8264c1
David Castro-Perez <david.castro.dcp@gmail.com>
2019-09-22 23:57:49 +0100

    Syntax seems to be working

--- examples/Mergesort/Mergesort.hs
+++ examples/Mergesort/Mergesort.hs
@@ -4,19 +4,35 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Mergesort where
 
-import Language.SPar.Skel
+import Control.CArr.CSyn
+import Language.SPar.Skel ( (:->), (:=>), AnnStrat, prim, fix, annotate, kfix, ann )
 
-type T a = Either a (a, a)
+-- type T a = Either a (a, a)
+
+merge :: CAlg f => f ([Int], [Int]) [Int]
+merge = cfun $ \x -> prim "merge" .@ var x
 
 msort :: CAlg f => f [Int] [Int] -> f [Int] [Int]
-msort ms = (vsize &&& id) >>>
-  if fst <= 1
-  then snd
-  else (fst &&& ((fst / 2) &&& snd))
-        >>> second ((vtake >>> ms) &&& (vdrop >>> ms))
-        >>> doMerge
-  where
-    doMerge = prim "merge"
+msort ms = cfun $ \x ->
+  alet (vsize $ var x) $ \sz ->
+  if var sz <= 1
+  then var x
+  else
+    alet (var sz / 2) $ \sz2 ->
+    alet (ms .@ vtake (var sz2) (var x)) $ \xl ->
+    alet (ms .@ vdrop (var sz2) (var x)) $ \xr ->
+    merge .@ pair (var xl, var xr)
+
+
+--msort :: CAlg f => f [Int] [Int] -> f [Int] [Int]
+--msort ms = (vsize &&& id) >>>
+--  if fst <= 1
+--  then snd
+--  else (fst &&& ((fst / 2) &&& snd))
+--        >>> second ((vtake >>> ms) &&& (vdrop >>> ms))
+--        >>> doMerge
+--  where
+--    doMerge = prim "merge"
 
 seqMsort :: [Int] :-> [Int]
 seqMsort = fix msort
--- src/Control/CArr/CSyn.hs
+++ src/Control/CArr/CSyn.hs
@@ -1,27 +1,45 @@
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE IncoherentInstances #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE MonoLocalBinds #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE IncoherentInstances #-}
+
 module Control.CArr.CSyn
   ( Var
   , (:<:)(..)
   , alet
-  , ($)
+  , (.$)
   , var
-  , fun
+  , Fun
+  , cfun
+  , (.@)
   , pair
   , fst
   , snd
   , acase
   , inl
   , inr
-  ) where
+  , ifThenElse
+  , vget
+  , vdrop
+  , vtake
+  , vsize
+  , (Prelude.$)
+  , X.CArrCmp(..)
+  , Prelude.Num(..)
+  , Prelude.Fractional(..)
+  , CAlg
+  , Either
+  , Int
+) where
 
--- import qualified Prelude
+import qualified Prelude
 
 import Data.C
 import qualified Control.CCat as X
@@ -30,7 +48,7 @@ import Control.CArr ( CAlg )
 
 newtype Var ctx a = Var { unVar :: forall t. CAlg t => t ctx a }
 
-class (CVal ctx, CVal ctx') => ctx :<: ctx' where
+class (CVal ctx, CVal ctx') => ctx :<: ctx' | ctx' -> ctx where
   sub :: CAlg t => t ctx' ctx
 
 instance CVal ctx => ctx :<: ctx where
@@ -39,21 +57,29 @@ instance CVal ctx => ctx :<: ctx where
 instance (CVal a, ctx :<: ctx') => ctx :<: (a, ctx') where
   sub = X.snd X.>>> sub
 
+instance
+  (CVal ctx, CVal ctx'', ctx :<: ctx', ctx' :<: ctx'') =>
+  ctx :<: ctx'' where
+  sub = sub X.>>> sub @ctx @ctx'
+
 var :: (CAlg t, CVal a, ctx :<: ctx') => Var ctx a -> t ctx' a
 var v = sub X.>>> unVar v
 
 type Fun t ctx a b = forall ctx'. ctx :<: ctx' => Var ctx' a -> t ctx' b
 
+cfun :: (CAlg t, CVal a, CVal b) => Fun t a a b -> t a b
+cfun f = f (Var X.id)
+
 alet :: (CAlg t, CVal ctx, CVal a, CVal b)
      => t ctx a -> Fun t ctx a b -> t ctx b
 alet x f = x X.&&& X.id X.>>> f (Var X.fst)
 
-($) :: (CAlg t, CVal ctx, CVal a, CVal b)
+(.$) :: (CAlg t, CVal ctx, CVal a, CVal b)
      => Fun t ctx a b -> t ctx a -> t ctx b
-f $ x = alet x f
+f .$ x = alet x f
 
-fun :: (CAlg t, CVal ctx, CVal a, CVal b) => t a b -> Var ctx a -> t ctx b
-fun f x = unVar x X.>>> f
+(.@) :: (CAlg t, CVal a, CVal b) => t a b -> forall ctx. CVal ctx => t ctx a -> t ctx b
+f .@ x = x X.>>> f
 
 pair :: (CAlg t, CVal ctx, CVal a, CVal b)
      => (t ctx a, t ctx b) -> t ctx (a, b)
@@ -80,3 +106,19 @@ inl x = x X.>>> X.inl
 
 inr :: (CAlg t, CVal ctx, CVal a, CVal b) => t ctx b -> t ctx (Either a b)
 inr x = x X.>>> X.inr
+
+ifThenElse :: (CAlg t, CVal ctx, CVal a) => t ctx Bool
+           -> t ctx a -> t ctx a -> t ctx a
+ifThenElse = X.ifThenElse
+
+vget :: (CAlg t, CVal ctx, CVal a) => t ctx Int -> t ctx [a] -> t ctx a
+vget i v = i X.&&& v X.>>> X.proj
+
+vtake :: (CAlg t, CVal ctx, CVal a) => t ctx Int -> t ctx [a] -> t ctx [a]
+vtake i v = i X.&&& v X.>>> X.vtake
+
+vdrop :: (CAlg t, CVal ctx, CVal a) => t ctx Int -> t ctx [a] -> t ctx [a]
+vdrop i v = i X.&&& v X.>>> X.vdrop
+
+vsize :: (CAlg t, CVal ctx, CVal a) => t ctx [a] -> t ctx Int
+vsize v = v X.>>> X.vsize
