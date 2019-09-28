4cb35a6c749a1a7f9b7ad1112965e15dc7494b8a
David Castro-Perez <david.castro.dcp@gmail.com>
2019-09-28 01:56:46 +0100

    Partial solution

--- examples/MapRed/ParMap.hs
+++ examples/MapRed/ParMap.hs
@@ -8,24 +8,24 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RebindableSyntax #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
-module Main where
+module ParMap where
 
-import qualified Prelude as P
+--import qualified Prelude as P
 import Control.CArr.CSyn
-import Language.SPar.Skel ( (:=>), emptyASt, compileAsLib )
-import Control.Monad.CGen
-import System.Environment
+--import Language.SPar.Skel ( (:=>), emptyASt, compileAsLib )
+--import Control.Monad.CGen
+--import System.Environment
 
 --timesBench :: CAlg f => f (TProd 32 [Int]) (TProd 32 [Int])
 --timesBench = cfun $ pmap (psize @32) (prim "sum")
 
-scalarProd :: [Int] :=> [Int]
+scalarProd :: CAlg f => f [Int] [Int]
 scalarProd = cfun $ \x ->
   vlet (vsize x / 32) $
   \sz' -> let takeFun y = vtake sz' $ vdrop (y * sz') x in
-    vlet (psplit @15 takeFun) $
-    \z -> vlet (smap @15 (prim "prod") z) $
-          \t -> pfold @15 (prim "cat" @@ 0) t
+    vlet (ssplit @5 takeFun) $
+    \z -> vlet (pmap @5 (prim "prod") z) $
+          \t -> pfold @5 (prim "cat") t
 
-main :: P.IO ()
-main = withProgName "ParMap" (generateFile emptyASt "ParMap" $ P.pure () P.>> compileAsLib "scalarProd" P.mempty scalarProd)
+--main :: P.IO ()
+--main = withProgName "ParMap" (generateFile emptyASt "ParMap" $ P.pure () P.>> compileAsLib "scalarProd" P.mempty scalarProd)
