e071ba613c017b4a488fa4d4bdbf07249525561a
dcastrop <d.castro-perez@imperial.ac.uk>
2019-10-01 18:13:43 +0100

    Fixes + Quicksort example

---
+++ examples/Quicksort/Quicksort.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Quicksort where
+
+import Control.CArr.CSyn
+import Language.SPar.Skel ( (:=>) )
+
+filter :: forall f ctx. (CAlg f, CVal ctx) => f ctx [Int] -> f ctx ([Int], [Int])
+filter = prim "filter"
+
+qsort :: forall f. (CAlg f, CArrFix f) => Int -> f [Int] [Int]
+qsort n = fix n $ \qs x ->
+  if vsize x <= 1
+  then x
+  else vlet (filter x) $ \lr ->
+    vlet (par qs $ fst lr) $ \xl ->
+    vlet (par qs $ snd lr) $ \xr ->
+    prim "cat" $ pair (xl, xr)
+
+parMsort0 :: [Int] :=> [Int]
+parMsort0 = qsort 0
+
+parMsort1 :: [Int] :=> [Int]
+parMsort1 = qsort 1
+
+parMsort2 :: [Int] :=> [Int]
+parMsort2 = qsort 2
+
+parMsort3 :: [Int] :=> [Int]
+parMsort3 = qsort 3
+
+parMsort4 :: [Int] :=> [Int]
+parMsort4 = qsort 4
+
+parMsort5 :: [Int] :=> [Int]
+parMsort5 = qsort 5
+
+parMsort6 :: [Int] :=> [Int]
+parMsort6 = qsort 6
+
+parMsort7 :: [Int] :=> [Int]
+parMsort7 = qsort 7
+
+parMsort8 :: [Int] :=> [Int]
+parMsort8 = qsort 8
