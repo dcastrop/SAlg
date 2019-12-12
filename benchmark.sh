8ce52a964ad6c2556bdb8e3867750b91b49eeb3b
dcastrop <d.castro-perez@imperial.ac.uk>
2019-12-12 12:42:34 +0000

    benchamarking script

--- benchmark.sh
+++ benchmark.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
+me=`basename "$0"`
+
 sbuild () {
   echo "+ stack exec -- session-arrc $1.hs"
   stack exec -- session-arrc $1.hs
-  echo "+ gcc $1.c main.c -pthread -lm -o bench"
-  gcc $1.c main.c -pthread -lm -o bench
+  echo "+ gcc $1.c main.c -DREPETITIONS=${REPETITIONS} -pthread -lm -o bench"
+  gcc $1.c main.c -DREPETITIONS=${REPETITIONS} -pthread -lm -o bench
 }
 
-REPETITIONS=${1:-"50"}
+MAXCORES=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
+NCORES=${1:-"${MAXCORES}"}
+NCORES=$((${NCORES}>=${MAXCORES} ? ${MAXCORES} : ${NCORES}))
 
-echo "REPETITIONS=${REPETITIONS}"
+REPETITIONS=${2:-"50"}
+
+echo "# Running ./${me} CORES REPETITIONS"
+echo "#    with CORES       = ${NCORES}"
+echo "#     and REPETITIONS = ${REPETITIONS}"
 
 DIRS="DotProd FFT Mergesort Quicksort ScalarMulMat"
 
-pushd examples
-
 for dir in ${DIRS}
 do
-  pushd ./${dir}
+  pushd ./examples/${dir} > /dev/null
+  echo "+ pushd ./examples/${dir}"
   GT=$(sbuild ${dir} | tee /dev/tty | sed -n 5p)
   echo "# Generating global type for: ${GT}"
   echo "+ stack exec -- session-arrc --infer ${GT} ${dir}.hs"
   stack exec -- session-arrc --infer ${GT} ${dir}.hs
-  popd
+  cat ${dir}_${GT}.mpst
+  ./run.sh ${NCORES}
+  popd > /dev/null
 done
-
-popd
 
